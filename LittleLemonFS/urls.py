35bcd6d237a319676b3f0ef96c64cad7ffed3ef2
Almaz Berhe <zamlatak@gmail.com>
2024-05-03 13:47:24 +0100

    added single menu item endpoint

--- LittleLemonFS/urls.py
+++ LittleLemonFS/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about", views.about, name="about"),
     path("menu", views.menu, name="menu"),
+    path("menu-item/<int:pk>", views.display_menu_item, name="menu_item"),
     path("book", views.BookingView.as_view(), name="book"),
     path("bookings", views.BookingsView.as_view(), name="bookings"),
 ]
